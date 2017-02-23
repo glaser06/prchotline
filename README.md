05b74137039b8be89587257544082e9a76932edf
zirenz <zirenz@andrew.cmu.edu>
2017-02-23 18:47:23 -0500

    added location, itemlocation scopes and stuff and migrations

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+This is a rails app that streamlines PRC's recycling hotline. 
 
 Things you may want to cover:
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,16 @@
 class Location < ApplicationRecord
 
-  
+  has_many :item_locations
+  has_many :items, through: :item_locations
+
+  validates :name, :address, :city, :zipcode, :state, presence: true
+  validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+
+  scope :alphabetical , -> { order('name') }
+  scope :active , -> { where(active: true) }
+  scope :inactive , -> { where(active: false) }
+  scope :search_zipcode, -> (zip) { where("zip=?", zip ) }
+
+  scope :by_zipcode, -> { order('zipcode') }
 
 end
