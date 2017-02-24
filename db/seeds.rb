f4d3dcc81a4916988b2fe864ba41cc11ae4bcc31
Rebecca Kern <rkern@andrew.cmu.edu>
2017-02-24 12:49:54 -0500

    added more objects to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 County.create(name: 'Alleghey')
 County.create(name: 'Bucks')
+
+Item.create(name: 'Paint', description: 'Paint needs to be disposed of properly to protect the environment.', active: true)
+Item.create(name: 'Television', description: 'Nothing right now', active: true)
+Item.create(name: 'Shredded Paper', description: 'Nothing right now', active: false)
+
+Location.create(name: 'Best Buy', address: '123 Main Street', city: 'Lancaster', phone: '4123211234', website: 'bestbuy.com', zipcode: '15123', state: 'PA')
+Location.create(name: 'Giant Eagle', address: '321 Hello Street', city: 'Pittsburgh', phone: '4123214567', website: 'gianteagle.com', zipcode: '15289', state: 'PA')
