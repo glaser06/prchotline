71a1bfdf4c91b1625db9091c19d4ca82bfe9b48e
Rebecca Kern <rkern@andrew.cmu.edu>
2017-03-26 11:49:12 -0400

    paginated validations

--- app/controllers/validations_controller.rb
+++ app/controllers/validations_controller.rb
@@ -10,7 +10,7 @@ class ValidationsController < ApplicationController
       if params[:county]
         countyId = params[:county]
         @countyName = County.find(countyId).name
-        @locations = Location.all.for_county(countyId).alphabetical
+        @locations = Location.all.for_county(countyId).alphabetical.paginate(:page => params[:page]).per_page(10)
       end
   end
 
