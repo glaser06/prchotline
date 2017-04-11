6979bcab84bcb007b6b6cf7054628be3d5de381c
Sai Dhulipalla <said@andrew.cmu.edu>
2017-04-11 17:58:45 -0400

    search in nav bar in two rows

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -14,7 +14,6 @@ class MainController < ApplicationController
     disposition = params[:disposition]
     county = params[:county]
     item = params[:item]
-    method = params[:method]
     type = params[:type]
     prc = params[:prcCall]
     dep = params[:depCall]
@@ -50,56 +49,5 @@ class MainController < ApplicationController
     @vals = session[:value]
   end
 
-
-
-    def index
-      @items = Item.all
-      @locations = []
-      @errors = " "
-      if params[:county] && params[:item]
-        @errors = ""
-        qCounty = params[:county]
-        qItem = params[:item]
-        county = County.for_name(qCounty.capitalize)
-
-        item = Alias.for_name(qItem.downcase)
-
-        if item.blank?
-          @errors += "Could not find #{params[:item]}"
-          return
-        end
-        @item = Item.find(item.first.item_id)
-        if county.blank?
-          @errors += "#{params[:county]} does not exist"
-          return
-        end
-        @county = county[0]
-        if params[:zip] != ""
-          qZip = params[:zip]
-          # i,l,c = search(qItem, qCounty, qZip)
-
-          coords = Geocoder.coordinates(qZip)
-          @locations1 = Address.near(coords,50)
-
-          @locations = @item.addresses.near(coords,50).paginate(:page => params[:page]).per_page(10)
-          # @locations = @item.locations.active.addresses.active.for_zipcode(qZip).alphabetical
-
-
-        else
-
-          coords = Geocoder.coordinates("#{@county.name} County")
-          @locations = @item.addresses.near(coords, 50).paginate(:page => params[:page]).per_page(10)
-          # @locations = @item.locations.active.for_county(@county.id).alphabetical
-
-
-        end
-        contexts = []
-        @locations.each do |loc|
-          context = ItemLocation.active.for_item(@item.id).for_location(loc.id).first
-          contexts.push(context)
-        end
-        @contexts = contexts
-      end
-    end
 
 end
