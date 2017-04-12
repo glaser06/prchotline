9ec75b4923da856e5a481f24b623db72e78b7ee8
Sai Dhulipalla <said@andrew.cmu.edu>
2017-04-12 13:12:52 -0400

    design and routes updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,9 @@ Rails.application.routes.draw do
   get 'main/index'
   get 'search' => 'main#index', as: :search
   get 'newCall' => 'main#newCall', as: :newCall
-  get 'search' => 'application#index', as: :s
-
   get 'validations' => 'validations'
 
   post 'clear_form' => 'main#clear_form'
-
   post 'newCall' => 'main#newCall'
   post 'submit_form' => 'main#submit_form'
   post 'clear_form' => 'main#clear_form'
