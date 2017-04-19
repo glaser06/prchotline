4f10e0c7fb125335743daf4102f1ce12b7489e46
zirenz <zirenz@andrew.cmu.edu>
2017-04-19 11:15:52 -0400

    minor fixes everywhere

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,9 +4,57 @@ class LocationsController < ApplicationController
   # GET /locations
   # GET /locations.json
   def index
+    # @l = Location.all.to_a
+    # @locations = Location.all.paginate(:page => params[:page]).per_page(20)
+    # @item_locations = ItemLocation.by_item.all
     @l = Location.all.to_a
     @locations = Location.all.paginate(:page => params[:page]).per_page(20)
     @item_locations = ItemLocation.by_item.all
+
+    if params[:validate] && params[:validate] != ""
+      puts "stuff"
+    end
+    # @locations = []
+    @countyName = ""
+    @items = Item.all
+    @counties = County.all
+    @item_locations = ItemLocation.all
+    locations = []
+    if params[:county]
+      @county = County.where(name: params[:county]).first
+      if not @county.nil?
+        @countyName = @county.name
+        @countyId = @county.id
+        locations = Location.all.for_county(@countyId)
+
+      else
+        locations = Location.all
+        @locations = "errorMessage"
+      end
+    else
+      locations = Location.all
+    end
+    if params[:sortby]
+      sort = params[:sortby]
+      if sort == "county"
+        @locations = locations.by_county.alphabetical.paginate(:page => params[:page]).per_page(10)
+        # @locations = locations.alphabetical.paginate(:page => params[:page]).per_page(10)
+      elsif sort == "name"
+        @locations = locations.alphabetical.paginate(:page => params[:page]).per_page(10)
+      elsif sort == "verified"
+        @locations = locations.by_updated.paginate(:page => params[:page]).per_page(10)
+      elsif sort == "city"
+        @locations = locations.by_city.alphabetical.paginate(:page => params[:page]).per_page(10)
+      elsif sort == "zipcode"
+        @locations = locations.by_zipcode.alphabetical.paginate(:page => params[:page]).per_page(10)
+      elsif sort == "active"
+        @locations = locations.by_active.alphabetical.paginate(:page => params[:page]).per_page(10)
+      end
+      puts "woah it works"
+
+    else
+      @locations = locations.alphabetical.paginate(:page => params[:page]).per_page(10)
+    end
   end
 
   # GET /locations/1
