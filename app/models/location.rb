e7ec736830c3152d39f03d1b674d8a18574fdf45
zirenz <zirenz@andrew.cmu.edu>
2017-04-26 18:46:39 -0400

    added delete to each model show page

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,9 +2,9 @@ class Location < ApplicationRecord
 
   before_save :reformat_phone
 
-  has_many :item_locations
+  has_many :item_locations, dependent: :destroy
   has_many :items, through: :item_locations
-  has_many :addresses, :autosave => true
+  has_many :addresses, :autosave => true, dependent: :destroy
 
   accepts_nested_attributes_for :item_locations, reject_if: lambda { |item_location| item_location[:item_id].blank? }, allow_destroy: true
   accepts_nested_attributes_for :addresses, reject_if: lambda { |addr| addr[:address].blank? }, allow_destroy: true
