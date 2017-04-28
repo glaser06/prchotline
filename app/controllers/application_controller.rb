c2e01db67372cfcb19d79a4f36c18e7236db0ffb
Sai Dhulipalla <said@andrew.cmu.edu>
2017-04-28 15:32:03 -0400

    password app.rb made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
+require 'csv'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with :name => 'prchotline', :password => 'recycle'
+
+  results = client.search("credentials.csv")
+  if results.matches.count > 0
+    path = results.matches.first.resource.path_lower
+    file_contents = ""
+    file = client.download(path) do |chunk|
+      file_contents << chunk
+    end
+    contentArr = file_contents.split(',')
+    user = file_contents[0].squish
+    pass = file_contents[1].squish
+    http_basic_authenticate_with :name => user, :password => pass
+  end
+
 
 end
--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,16 +1,16 @@
 class SettingsController < ApplicationController
 
 
-  def index
-    puts "index method"
-    
-
+  def change
+    puts "change"
+    client = DropboxApi::Client.new
+    file_content = params[:user], params[:password]
+    client.upload "credentials.csv", file_content, :mode => :overwrite
+    redirect_to settings_path
   end
 
   def downloadPRC
   send_file("PRCcall_stats.csv", type: "text/csv", disposition: "attachment")
-
-    # redirect_to "/"
   end
 
 end
