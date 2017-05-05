a200af408d25f0421982a90582f2d61e29f48e38
Sai Dhulipalla <said@andrew.cmu.edu>
2017-05-05 14:08:19 -0400

    views and models code formatted

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,52 +1,37 @@
 class Address < ApplicationRecord
-  # before_save :reformat_phone
 
   belongs_to :location, required: false
   belongs_to :county
 
-
-
   validates :address, :county, presence: true
-
 
   scope :active, -> { where(active: true) }
   scope :inactive, -> { where(active: false) }
   scope :for_zipcode, -> (zip) { where("zipcode=?", zip ) }
   scope :by_zipcode, -> { order('zipcode') }
   scope :for_county, -> (id) { where('county_id=?', id) }
-
-
   scope :by_name, -> { joins(:location).order('locations.name')}
   scope :by_verified, -> { joins(:location).order('locations.updated_at')}
-
   scope :by_county, -> { order('county_id') }
   scope :by_city, -> { order('city') }
   scope :by_active, -> {order('active DESC')}
 
-
-
-
-
-
   geocoded_by :full_address
-  # after_validation :geocode
-  # after_validation :geocode
-  def require_one_address
-    puts location.addresses.count > 1
-    return location.addresses.count > 1
 
+  def require_one_address
+    location.addresses.count > 1
   end
+
   def full_address
-    address = "#{self.address}, #{self.city}, PA #{self.zipcode}"
-    return address
+    "#{self.address}, #{self.city}, PA #{self.zipcode}"
   end
 
   def street_address
-    return "#{self.address}"
+    "#{self.address}"
   end
 
   def extra_details
-    return "#{self.city}, PA #{self.zipcode}"
+    "#{self.city}, PA #{self.zipcode}"
   end
 
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -13,27 +13,18 @@ class Location < ApplicationRecord
   validates :name, presence: true
   validates :addresses, presence: true
   validates_format_of :phone, with: /\A\(?\d{3}\)?[-. ]?\d{3}[-.]?\d{4}\z/, message: "should be 10 digits (area code needed) and delimited with dashes only", allow_blank: true
-  #TO DO validates format of website
-
   validate :require_one_address
 
   scope :alphabetical , -> { order('name') }
   scope :active, -> { where(active: true) }
   scope :inactive, -> { where(active: false) }
   scope :for_county, -> (id) {joins(:addresses).where(addresses: {county_id: id})}
-
   scope :for_item, -> (id) {joins(:item_locations).where(item_locations: {item_id: id})}
-
   scope :by_county, -> {joins(:addresses).order("addresses.county_id")}
   scope :by_city, -> {joins(:addresses).order("addresses.city")}
   scope :by_zipcode, -> {joins(:addresses).order("addresses.zipcode")}
   scope :by_updated, ->  {order('updated_at')}
   scope :by_active, ->  {order('active')}
-
-  # scope :for_zipcode, -> (zip) { where("zipcode=?", zip ) }
-  # scope :by_zipcode, -> { order('zipcode') }
-  # scope :by_county, ->  { includes(:county).order('counties.name') }
-  # scope :for_county, -> (id) { where('counties_id=?', id) }
 
   def item_locations_for_form
     collection = item_locations.where(location_id: id)
@@ -52,14 +43,12 @@ class Location < ApplicationRecord
 
   end
 
-
   private
 
   def require_one_address
-
     errors.add(:base, "You must provide at least one address") if addresses.size < 1
-
   end
+
   def rejectable?(addr)
     if addr[:address].blank?
       return true
@@ -69,11 +58,10 @@ class Location < ApplicationRecord
     end
   end
 
-
   def reformat_phone
-    phone = self.phone.to_s  # change to string in case input as all numbers
-    phone.gsub!(/[^0-9]/,"") # strip all non-digits
-    self.phone = phone       # reset self.phone to new string
+    phone = self.phone.to_s
+    phone.gsub!(/[^0-9]/,"")
+    self.phone = phone
   end
 
 end
