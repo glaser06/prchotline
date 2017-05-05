09e4b10dc9a4be49f7534ea3b90d1e3d762bead7
Sai Dhulipalla <said@andrew.cmu.edu>
2017-05-05 14:08:19 -0400

    views and models code formatted

--- app/models/county.rb
+++ app/models/county.rb
@@ -2,15 +2,12 @@ class County < ApplicationRecord
 
   before_save :reformat_county_name
 
-  # Relationships
   has_many :item_counties, dependent: :destroy
   has_many :items, through: :item_counties
   has_many :addresses, dependent: :destroy
 
-  # Validations
   validates_presence_of :name
 
-  # Scopes
   scope :alphabetical, -> { order('name') }
   scope :for_name, -> (name) { where('name=?',name) }
 
